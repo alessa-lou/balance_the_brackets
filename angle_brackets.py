efa7aff645b9bd67eb57f269c0fa900751fd9598
alessa-lou <alessa_hardwick@live.co.uk>
2020-12-18 14:13:33 +0000

    removed comments

--- angle_brackets.py
+++ angle_brackets.py
@@ -2,7 +2,6 @@ class Angles:
 
     def check(self, angles):
         angles = list(angles)
-        # print(angles)
         counter = 0
         for i in angles:
             if i == "<":
@@ -14,7 +13,6 @@ class Angles:
                 angles.insert(0, "<")
                 angles.append(">")
                 print("printing angles 3", "".join(angles))
-                # return "".join(angles)
 
         if counter > 0:
             angles.append(">" * counter)
@@ -29,36 +27,3 @@ class Angles:
         else:
             print("printing angles 3", "".join(angles))
             return "".join(angles)
-
-
-
-
-
-    # left = []
-    # right = []
-    # for i in angles:
-    #     if i == "<":
-    #         left.append("<")
-    #     elif i == ">":
-    #         right.append(">")
-    # print(left)
-    # print(right)
-    # left_brackets = left.count("<")
-    # right_brackets = right.count(">")
-    # if left_brackets % 2 == 0 and right_brackets % 2 == 0:
-    #     if angles[-1] == "<":
-    #         angles.append(">")
-    #         print("printing angles 1", angles)
-    #         return angles
-    #     elif angles[0] == ">":
-    #         angles.insert(0, "<")
-    #         print("printing angles 2", angles)
-    #         return angles
-    #     else:
-    #         print("printing angles 3", angles)
-    #         return angles
-    # else:
-    #     print("lol no")
-    #     return "no"
-
-# checking this: "<<>><"
